4880015947276867e5ec17361487c8bf18c26c2e
Dmitry <sc.dmitry@gmail.com>
2024-03-27 10:15:26 +0100

    After arrow funstions applied

--- splitter_files/js/splitter.js
+++ splitter_files/js/splitter.js
@@ -1,88 +1,69 @@
 class Splitter {
+  panes = [];
+  isDragging = false;
+  startX = 0; 
+  startY = 0;
+  shiftX = 0;
+  shiftY = 0;
+
   constructor(arg) {
-    this.panes = [];
-  
-    this.isDragging = false;
-    this.startX = 0; 
-    this.startY = 0;
-    this.shiftX = 0;
-    this.shiftY = 0;
-
-    this.#getDom(arg).addEventListener('mousedown', this.startDrag);
-
+    this.getDom(arg).addEventListener('mousedown', this.startDrag.bind(this));
   }
 
-  #getDom(arg) {
+  getDom(arg) {
     const element = arg instanceof HTMLElement ? arg : document.getElementById(arg)
-    if(element == null) console.log('No element${arg} found method: #getdom()');
+    if (element == null) console.log(`No element ${arg} found method: getDom()`);
 
-    // console.log('Element${arg} found method: #getdom()');
     return element;
   }
 
-  addPane = (arg, kx, ky) => {
-    this.panes.push(new ResizePane(this.#getDom(arg), kx, ky));
+  addPane(arg, kx, ky) {
+    this.panes.push(new ResizePane(this.getDom(arg), kx, ky));
     console.log('Num of panes: ', this.panes.length);
   }
 
-
-  
   startDrag = (event) => {
     this.isDragging = true;
     this.startX = event.clientX;
     this.startY = event.clientY;
 
     console.log(this.panes.length);
-    for (key in this.panes) {
+    for (let key in this.panes) {
       this.panes[key].saveInitSize();
-      }
+    }
 
     document.addEventListener('mousemove', this.handleDrag);
     document.addEventListener('mouseup', this.stopDrag);
   }
 
-  handleDrag(event) {
-    if (! this.isDragging) return;
+  handleDrag = (event) => {
+    if (!this.isDragging) return;
 
     const currentX = event.clientX;
     const currentY = event.clientY;
 
-    this.shiftX = currentX -  this.startX;
-    this.shiftY = currentY -  this.startY;
+    this.shiftX = currentX - this.startX;
+    this.shiftY = currentY - this.startY;
 
-    // console.log('Horizontal shift:', shiftX, 'Vertical shift:', shiftY);
- 
-
-    // You can perform further actions here based on the shift
-    // For example, update the position of an element
-    console.log('panes num ',this.panes.lenght);
-    for (key in this.panes) {
-      this.panes[key].updateSize( this.shiftX,  this.shiftY);
-      
-      console.log('key ${key}');
-      }
-
-
-
+    console.log('panes num ', this.panes.length);
+    for (let key in this.panes) {
+      this.panes[key].updateSize(this.shiftX, this.shiftY);
+      console.log(`key ${key}`);
+    }
   }
 
-  stopDrag() {
-    if (! this.isDragging) return;
+  stopDrag = () => {
+    if (!this.isDragging) return;
 
     this.isDragging = false;
     document.removeEventListener('mousemove', this.handleDrag);
     document.removeEventListener('mouseup', this.stopDrag);
 
-    // Example: Reset shift values after drag
+    // Reset shift values after drag
     this.shiftX = 0;
     this.shiftY = 0;
   }
-
-
-
 }
-
-
 
 
 
